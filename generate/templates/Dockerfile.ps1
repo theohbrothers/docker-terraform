403fad674c8903d6d46b1681df4353f1aa9a28f2
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-03-11 22:58:30 +0000

    Enhancement: Add validation for `sops`

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -27,7 +27,7 @@ RUN apk add --no-cache libvirt-client
 
 if ( $VARIANT['_metadata']['components'] -contains 'sops' ) {
     if ( $VARIANT['_metadata']['distro'] -eq 'alpine' -and $VARIANT['_metadata']['distro_version'] -in @('3.6', '3.5', '3.4', '3.3') ) {
-        @"
+@"
 # Fix generic certification validation errors in alpine 3.5: https://github.com/docker-library/official-images/issues/2773#issuecomment-350431934
 RUN apk add --no-cache ca-certificates
 
@@ -35,15 +35,23 @@ RUN apk add --no-cache ca-certificates
 # RUN apk add --no-cache libressl
 
 # Fix wget error 'wget: SSL/TLS certificate is not being validated!' in alpine:3.5, use curl instead: https://github.com/docker-library/official-images/issues/2773
-RUN apk add --no-cache curl \
-    && curl -sL https://github.com/mozilla/sops/releases/download/v3.7.1/sops-v3.7.1.linux > /usr/local/bin/sops && chmod +x /usr/local/bin/sops \
-    && apk del curl
+RUN set -eux; \
+    apk add --no-cache curl; \
+    curl -sSL https://github.com/mozilla/sops/releases/download/v3.7.1/sops-v3.7.1.linux > /usr/local/bin/sops; \
+    chmod +x /usr/local/bin/sops; \
+    sha256sum /usr/local/bin/sops | grep '^185348fd77fc160d5bdf3cd20ecbc796163504fd3df196d7cb29000773657b74 '; \
+    sops --version; \
+    apk del curl
 
 
 "@
     }else {
-        @"
-RUN wget -qO- https://github.com/mozilla/sops/releases/download/v3.7.1/sops-v3.7.1.linux > /usr/local/bin/sops && chmod +x /usr/local/bin/sops
+@"
+RUN set -eux; \
+    wget -qO- https://github.com/mozilla/sops/releases/download/v3.7.1/sops-v3.7.1.linux > /usr/local/bin/sops; \
+    chmod +x /usr/local/bin/sops; \
+    sha256sum /usr/local/bin/sops | grep '^185348fd77fc160d5bdf3cd20ecbc796163504fd3df196d7cb29000773657b74 '; \
+    sops --version
 
 
 "@
